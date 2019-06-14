aaec35ba3555f346382d2db69c1deb82eaf7ccb3
Spencer <spenjlee@gmail.com>
2019-06-14 10:19:16 -0700

    Set up two_sum testing environment

--- instructions.rb
+++ instructions.rb
@@ -1,18 +1,4 @@
-Remove dups
-Array has a uniq method that removes duplicates from an array. It returns the unique elements in the order in which they first appeared:
 
-[1, 2, 1, 3, 3].uniq # => [1, 2, 3]
-Write your own version of this method called my_uniq; it should take in an Array and return a new array.
-
-Two sum
-Write a new Array#two_sum method that finds all pairs of positions where the elements at those positions sum to zero.
-
-NB: ordering matters. We want each of the pairs to be sorted smaller index before bigger index. We want the array of pairs to be sorted "dictionary-wise":
-
-[-1, 0, 2, -2, 1].two_sum # => [[0, 4], [2, 3]]
-[0, 2] before [2, 1] (smaller first ele
-ments come first)
-[0, 1] before [0, 2] (then smaller second elements come first)
 My Transpose
 To represent a matrix, or two-dimensional grid of numbers, we can write an array containing arrays which represent rows:
 
---
+++ two_sum/spec/two_sum_spec.rb
@@ -0,0 +1,17 @@
+require 'two_sum'
+require 'rspec'
+
+# Two sum
+# Write a new Array#two_sum method that finds all pairs of positions where the elements at those positions sum to zero.
+
+# NB: ordering matters. We want each of the pairs to be sorted smaller index before bigger index. 
+# We want the array of pairs to be sorted "dictionary-wise":
+
+# [-1, 0, 2, -2, 1].two_sum # => [[0, 4], [2, 3]]
+# [0, 2] before [2, 1] (smaller first elements come first)
+# [0, 1] before [0, 2] (then smaller second elements come first)
+
+describe 'two_sum' do
+    it "pending test"
+
+end
