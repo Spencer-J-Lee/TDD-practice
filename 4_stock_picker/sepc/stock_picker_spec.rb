95d59dec3353463d828c0fded75284ec97e88ed1
Spencer <spenjlee@gmail.com>
2019-06-14 14:17:28 -0700

    Set up testing environment for stock_picker

---
+++ 4_stock_picker/sepc/stock_picker_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require 'stock_picker'
+
+# Stock Picker
+# Write a method that takes an array of stock prices (prices on days 0, 1, ...), 
+# and outputs the most profitable pair of days on which to first buy the stock and then sell the stock. 
+# Remember, you can't sell stock before you buy it!
+
+describe 'stock_picker' do
+
+end
