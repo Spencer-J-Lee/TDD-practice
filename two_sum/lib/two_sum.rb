0d45374c8aa6745c97504b09d55a61754f55a68c
Spencer <spenjlee@gmail.com>
2019-06-14 10:27:40 -0700

    Add zero pair finder functionality to two_sum

--- two_sum/lib/two_sum.rb
+++ two_sum/lib/two_sum.rb
@@ -1,2 +1,11 @@
 def two_sum(array)
+    zero_pairs = []
+
+    array.each_with_index do |num1, idx1|
+        array.each_with_index do |num2, idx2|
+            zero_pairs << [idx1, idx2] if idx2 > idx1 && (num1 + num2).zero?
+        end
+    end
+
+    zero_pairs
 end
