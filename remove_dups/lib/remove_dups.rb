38617d3e6c959b90a0293d55912b9c59f4a2d6fd
Spencer <spenjlee@gmail.com>
2019-06-14 10:02:02 -0700

    Add array duplicate removal functionality to my_uniq

--- remove_dups/lib/remove_dups.rb
+++ remove_dups/lib/remove_dups.rb
@@ -1,2 +1,5 @@
 def my_uniq(array)
+    uniq_array = []
+    array.each { |el| uniq_array << el unless uniq_array.include?(el) }
+    uniq_array
 end
--- remove_dups/spec/remove_dups_spec.rb
+++ remove_dups/spec/remove_dups_spec.rb
@@ -7,8 +7,10 @@ require 'remove_dups'
 # [1, 2, 1, 3, 3].uniq # => [1, 2, 3]
 # Write your own version of this method called my_uniq; it should take in an Array and return a new array.
 
-describe my_uniq do
+describe 'my_uniq' do
     subject(:array) { [1,2,1,3,3] }
 
-    
+    it "should remove duplicate elements from an array" do
+        expect(my_uniq(array)).to eq([1,2,3])
+    end
 end
