1ce3ae05581222993bfabf7da3dc73fe10326952
Spencer <spenjlee@gmail.com>
2019-06-14 14:50:39 -0700

    Add functionality for stock_picker to return most profitable days

--- 4_stock_picker/lib/stock_picker.rb
+++ 4_stock_picker/lib/stock_picker.rb
@@ -1,3 +1,19 @@
 def stock_picker(prices)
+    best_profit = 0
+    best_days   = nil
 
+    prices.each_with_index do |buy_price, buy_day|
+        prices.each_with_index do |sell_price, sell_day|
+            if sell_day > buy_day
+                profit = sell_price - buy_price
+
+                if profit > best_profit
+                    best_days   = [buy_day, sell_day]
+                    best_profit = profit
+                end
+            end
+        end
+    end
+
+    best_days
 end
